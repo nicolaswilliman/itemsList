1be53bf2aa94552cbe404895794ef615be6bc346
Nicolas Williman <williman.nicolas@gmail.com>
2020-01-16 00:42:00 -0300

    minor changes

--- web/src/index.php
+++ web/src/index.php
@@ -5,48 +5,68 @@
 	<title>Challenge</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+	<style>
+		li:hover {
+    		cursor: move;
+		}
+	</style>
 </head>
 
 <body>
+	<h1 id="title">
+	</h1>
 	<div id="content">
-		<h1>
-			Items
-		</h1>
-		<ul id="items">
 
-		</ul>
 	</div>
 	<script>
-		$(function(){
-			$.ajax({
-				url: 'get.php',
-				type: 'get',
-				success: response => {
-					let html = '';
-					$.each(JSON.parse(response), (key, data) => {
-						html += '<li class="item">' + data.desc + '</li>'
-					});
+		function getItems(){
+			$(() => {
+				$.ajax({
+					url: 'get.php',
+					type: 'get',
+					success: response => {
+						let html = '';
+						html += '<input type="button" id="create" value="Create item">';
+						html += '<ul id="items">';
+						$.each(JSON.parse(response), (key, data) => {
+							html += '<li class="item">' + data.desc + '</li>'
+						});
+						html += '</ul>';
 
-					$('#items').html(html);
-				},
-				error: (xhr, ajaxOptions, thrownError) => {
-					let errorMsg = 'Request failed: ' + xhr.responseText;
-					$('#content').html(errorMsg);
-				}
+						$('#content').html(html);
+						let items = $('#items');
+						items.sortable({
+							update: (event, ui) => {
+								let list = [];
+								$('#items li').each((idx, elem) => {
+									list[idx] = $(elem).html();
+								});
+							}
+						});
+						items.disableSelection();
+					},
+					error: (xhr, ajaxOptions, thrownError) => {
+						let errorMsg = 'Request failed: ' + xhr.responseText;
+						$('#content').html(errorMsg);
+					}
+				});
+
 			});
-			let items = $('#items');
-			items.sortable({
-				update: (event, ui) => {
-					let list = [];
-					$('#items li').each((idx, elem) => {
-						list[idx] = $(elem).html();
-					});
-				}
-			});
-    		$('#items').disableSelection();
+		}
+
+		getItems();
+
+		$('#create').click(() => {
+			let html = '';
+			html += '<span>Description:</span>';
+			html += '<input type="text" id="desc">';
+			html += '<br>';
+			html += '<span>Image:</span>';
+			html += '<input type="file" id="image">';
+			$('#content').html(html);
 		});
+
 	</script>
 </body>
-
 </html>
 
