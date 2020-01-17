6e731295c0578ae6ab9f987c402bd772d80d1120
Nicolas Williman <williman.nicolas@gmail.com>
2020-01-16 22:09:48 -0300

    mongodb connection works properly

--- web/www/src/test.php
+++ web/www/src/test.php
@@ -2,8 +2,15 @@
 require '../../vendor/autoload.php';
 use MongoDB\Client as Mongo;
 
-$connection = new Mongo("mongodb://mongo-db:27017");
+$connection = new Mongo("mongodb://root:root@mongo-db:27017");
 
-$collection = $connection->selectCollection('testdb', 'collection-name');
-var_dump($collection);
+try {
+	$dbs = $connection->listDatabases();
+} catch (\Exception $e) {
+	echo $e->getMessage();
+}
+
+
+//$collection = $connection->selectCollection('testdb', 'collection-name');
+//var_dump($collection);
 exit;
